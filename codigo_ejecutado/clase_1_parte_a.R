18e4ed2da3b248bbc4ae205d2b77b5a77de13683
pachedi <dapacheco.87@gmail.com>
2025-07-14 13:23:24 -0300

    actu_julio

---
+++ codigo_ejecutado/clase_1_parte_a.R
@@ -0,0 +1,94 @@
+
+# Acceder a la documentación
+?print
+
+# Tipos de dato
+
+a <- 10  
+
+print(a)
+
+class(a)
+
+b <- "Hola programación en R"
+
+class(b)
+
+c <- 5
+d <- 3
+
+c + d
+
+e <- "9"
+
+c + e
+
+as.numeric(e)
+
+c + e
+
+e <- as.numeric(e)
+class(e)
+
+c + e
+
+# Multiplicacion
+
+9 * 8
+
+# Resta
+
+10 - 5
+
+resultado_resta <- 10 - 5
+
+# Potencia
+
+3**2
+
+
+# Operadores logicos
+
+TRUE # 1
+FALSE # 0
+
+TRUE + TRUE 
+FALSE + FALSE
+TRUE + FALSE
+
+numero1 <- 20
+numero2 <- 30
+numero3 <- 20
+
+numero1 > numero2
+
+numero2 > numero1
+
+numero1 >= numero2  
+
+numero1 != numero2 
+numero1 != numero3
+
+numero1 == numero2
+numero1 == numero3 
+
+b <- 7.5
+
+class(b)
+
+v_num <- c(9,b,8,10)
+
+v_num[1]
+v_num[4]
+
+b <- 9
+
+v_num[2]
+
+
+
+
+
+
+
+
